20915b3ce7a55bec25505533ba10ed40da512fde
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-03-15 13:58:21 +0100

    Initial cache

---
+++ buildout/eggs/tilecloud-0.2dev_20131022-py2.7.egg/tilecloud/store/bsddb.py
@@ -0,0 +1,45 @@
+from itertools import imap
+
+from tilecloud import Tile, TileCoord, TileStore
+
+
+class BSDDBTileStore(TileStore):
+
+    def __init__(self, db, **kwargs):
+        self.db = db
+        TileStore.__init__(self, **kwargs)
+
+    def __contains__(self, tile):
+        return tile and str(tile.tilecoord) in self.db
+
+    def __len__(self):
+        return len(self.db)
+
+    def delete_one(self, tile):
+        key = str(tile.tilecoord)
+        if key in self.db:
+            del self.db[key]
+        return tile
+
+    def get_all(self):
+        for key, data in self.db.items():
+            tile = Tile(TileCoord.from_string(key), content_type=self.content_type, data=data)
+            yield tile
+
+    def get_one(self, tile):
+        try:
+            print self.db.keys()
+            tile.content_type = self.content_type
+            tile.data = self.db[str(tile.tilecoord)]
+            return tile
+        except KeyError:
+            return None
+
+    def list(self):
+        return imap(lambda s: Tile(TileCoord.from_string(s)), self.db.iterkeys())
+
+    def put_one(self, tile):
+        #print 777
+        #print tile.tilecoord#1/14/11
+        self.db[str(tile.tilecoord)] = getattr(tile, 'data', '')
+        return tile
